6ae3bec7c830400da0305a82e1297f775eca1b5f
Philipp Alber <philipp_j.alber@yahoo.com>
2019-10-18 20:31:53 +0200

    added routing

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,18 +4,40 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule, Routes } from '@angular/router';
 
 import { MatToolbarModule, MatIconModule } from '@angular/material';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { NavComponent } from './components/nav/nav.component';
+import { UrlShortenerComponent } from './components/url-shortener/url-shortener.component';
+import { PathNotFoundComponent } from './components/path-not-found/path-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'shorten',
+    component: UrlShortenerComponent
+  },
+  {
+    path: '',
+    redirectTo: '/shorten',
+    pathMatch: 'full'
+  },
+  {
+    path: '**',
+    component: PathNotFoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    UrlShortenerComponent,
+    PathNotFoundComponent
   ],
   imports: [
+    RouterModule.forRoot(appRoutes),
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
