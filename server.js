979023cc7d35c1274d4da4b4ded3a837cfd5b77a
Philipp Alber <philipp_j.alber@yahoo.com>
2019-10-21 20:53:23 +0200

    did some more api work, finished get at post request, added sqlite database for storing url models data

--- server.js
+++ server.js
@@ -1,14 +1,39 @@
 const express = require('express');
-const mysql = require('mysql');
+const sqlite3 = require('sqlite3');
+require('dotenv').config();
 
 const app = express();
-const port = 3000;
+const db = new sqlite3.Database('./database.db');
+
+db.run('CREATE TABLE IF NOT EXISTS urls ( id INTEGER PRIMARY KEY, url VARCHAR(255) NOT NULL, timeout SMALLINT NOT NULL, message VARCHAR(255) )');
 
 app.use(express.json());
 // app.use(express.static(__dirname + '/client'));
 
 app.post('/api', (req, res) => {
-    res.json({success: true});
+    db.run('INSERT INTO urls (url, timeout, message) VALUES (?, ?, ?)', [req.body.url, req.body.timeout, req.body.message], err => {
+        if (err) {
+            res.json({success: false});
+        } else {
+            res.json({success: true});
+        }
+    });
 });
 
-app.listen(port, () => console.log(`app listening on port ${port}`));
+app.get('/api', (req, res) => {
+    db.all('SELECT * FROM urls', [], (err, rows) => {
+        if (!err) {
+            res.json(rows);
+        }
+    });
+});
+
+app.get('/api/:id', (req, res) => {
+    db.get('SELECT * FROM urls WHERE id = ?', [req.params.id], (err, row) => {
+        if (!err) {
+            res.json(row);
+        }
+    });
+});
+
+app.listen(process.env.PORT || 3000, () => console.log(`app listening on port ${process.env.PORT}`));
